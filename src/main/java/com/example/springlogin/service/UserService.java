ed4135d347802f849eddd2d87b459e5d896a8678
Ananias Raphael <ananias.nobrega@gmail.com>
2021-04-22 22:10:14 -0300

    service

--- src/main/java/com/example/springlogin/service/UserService.java
+++ src/main/java/com/example/springlogin/service/UserService.java
@@ -1,9 +1,15 @@
 package com.example.springlogin.service;
 
+import com.example.springlogin.model.Role;
+import com.example.springlogin.model.User;
 import com.example.springlogin.repository.RoleRepository;
 import com.example.springlogin.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.HashSet;
 
 @Service
 public class UserService {
@@ -11,5 +17,28 @@ public class UserService {
     private UserRepository userRepository;
     private RoleRepository roleRepository;
     private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    public UserService(UserRepository userRepository, RoleRepository roleRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    public User findUserByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    public User findUserByUserName(String userName) {
+        return userRepository.findByUserName(userName);
+    }
+
+    public User saveUser(User user){
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        user.setActive(true);
+        Role userRole = roleRepository.findByRole("ADMIN");
+        user.setRoles(new HashSet<Role>(Arrays.asList(userRole)));
+        return userRepository.save(user);
+    }
 
 }
---
+++ src/main/java/com/example/springlogin/service/UserServiceTest.java
@@ -0,0 +1,4 @@
+package com.example.springlogin.service;
+
+public class UserServiceTest {
+}
