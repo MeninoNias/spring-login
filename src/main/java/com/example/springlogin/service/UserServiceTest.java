a9d08f4eacf77fff7f892fa6fecced074df2c98d
Ananias Raphael <ananias.nobrega@gmail.com>
2021-04-26 14:46:57 -0300

    create configuration packge

--- src/main/java/com/example/springlogin/service/UserServiceTest.java
+++ src/main/java/com/example/springlogin/service/UserServiceTest.java
@@ -1,4 +1,19 @@
 package com.example.springlogin.service;
 
+import com.example.springlogin.model.User;
+import com.example.springlogin.repository.RoleRepository;
+import com.example.springlogin.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+
+
 public class UserServiceTest {
+
+    private UserRepository userRepository;
+    private RoleRepository roleRepository;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    private UserService userServiceTest;
+    private User user;
+
 }
